774ea3edc03fb7dd73bb1cd52a93c4a66c2978da
rousseaualejo <rousseaualejo@gmail.com>
2023-02-15 06:38:39 -0300

    add App Skeleton

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorBuilder(
+      builder:(lightDynamic, darkDynamic) {
+        return MaterialApp(
+          title: 'Test',
+          debugShowCheckedModeBanner: false,
+
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightDynamic 
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkDynamic 
+          ),
+
+          home: Scaffold(
+            body: Container()
+          ),
+        );
+      }, 
+    );
+  }
+}
