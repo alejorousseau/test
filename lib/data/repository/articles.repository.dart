318650522c74afee1a1192b78ce4cab2341fdb7c
rousseaualejo <rousseaualejo@gmail.com>
2023-02-15 09:23:05 -0300

    get articles from cache and refresh

--- lib/data/repository/articles.repository.dart
+++ lib/data/repository/articles.repository.dart
@@ -1,28 +1,38 @@
 
 import 'package:dio/dio.dart';
-import 'package:technical_test/domain/models/article.model.dart';
+import 'package:technical_test/domain/models/article/article.cache.dart';
+import 'package:technical_test/domain/models/article/article.model.dart';
 
 abstract class IArticleRepository {
 
-  Future<List<Article>?> get();
+  Future<List<Article>?> get({bool isRefreshing = false});
 
 } 
 
 class ArticleRepository extends IArticleRepository {
   Dio dio;
+  ArticlesCache articlesCache;
 
-  ArticleRepository({ required this.dio });
+  ArticleRepository({ required this.dio, required this.articlesCache });
+
 
   @override
-  Future<List<Article>?> get() async {
+  Future<List<Article>?> get({bool isRefreshing = false}) async {
+
+    final ArticlesCache cache = await articlesCache.getCache();
+    if(cache.isArticlesListRetrieved && !isRefreshing){
+      return cache.articles;
+    }
+
     final res = await dio.get(
       '/search_by_date?query=mobile'
     );
 
     if( res.statusCode != 200 && res.statusCode != 200 ) return null;
-
-    final articleList = ArticlesList.fromJson( res.data );
-    return articleList.articles;
+    final articleList = ArticlesList.fromJson( res.data ).articles;
+    articlesCache.setNewArticles(newArticles: articleList);
+    
+    return articleList;
   }
 
 }
